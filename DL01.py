92f643a44ccc4e3777bb0261622f17ad6f5f2139
claw0ed <claw0ed@gmail.com>
2018-03-21 10:18:56 +0900

    2018.03.21

---
+++ DL01.py
@@ -0,0 +1,11 @@
+# DL01
+# 딥러닝 기초 - 텐서플로, 케라스
+
+# 딥러닝 개발환경
+# 1. 파이썬 3.5이상 또는 아나콘다 4.4이상
+# 2. 파이참 - 파이썬 venv 환경에서 실행하도록 설정
+# 3. 머신러닝/딥러닝 관련 패키지 설치
+# numpy scipy matplotlib pandas scikit-learn
+# spyder(과학계산) seaborn(시각화) h5py(hdf5) pillow(이미지)
+# tensorflow keras
+
